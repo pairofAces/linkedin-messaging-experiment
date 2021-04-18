8c7baf49bc734dd7cbcae4e2caa6e4e14b783489
pairofAces <karanchauhan16@gmail.com>
2021-04-17 23:53:05 -0400

    rendering chat props

--- chat-app/src/components/ChatFeed.jsx
+++ chat-app/src/components/ChatFeed.jsx
@@ -1,7 +1,15 @@
-const ChatFeed = (props) => {
-    // lets see what props are coming into the ChatFeed
-    console.log(props);
+import MessageForm from './MessageForm';
+import  MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
 
+
+const ChatFeed = (props) => {
+    // render the props, destructure
+    const { chats, activeChat, userName, messages } = props;
+
+    // create a chat variable, IF it exists, as well as the activeChat
+    const chat = chats && chats[activeChat]
+    
     return (
         <div>
             ChatFeed
