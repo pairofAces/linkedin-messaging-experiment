f45c56b1ed820436ece2480c4a63e46af16bdf97
pairofAces <karanchauhan16@gmail.com>
2021-04-17 22:56:59 -0400

    ChatFeed div is rendering

--- chat-app/src/components/ChatFeed.jsx
+++ chat-app/src/components/ChatFeed.jsx
@@ -1,3 +1,12 @@
-const ChatFeed = () => {
-    
+const ChatFeed = (props) => {
+    // lets see what props are coming into the ChatFeed
+    console.log(props);
+
+    return (
+        <div>
+            ChatFeed
+        </div>
+    )
 }
+
+export default ChatFeed;
