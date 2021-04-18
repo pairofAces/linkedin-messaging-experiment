4912e9726e3e174a0b9f165165f6b704f946e27a
pairofAces <karanchauhan16@gmail.com>
2021-04-18 14:26:10 -0400

    working on rendering the messages

--- chat-app/src/components/ChatFeed.jsx
+++ chat-app/src/components/ChatFeed.jsx
@@ -13,9 +13,33 @@ const ChatFeed = (props) => {
     // console.log(chat, userName, messages)
 
     const renderMessages = () => {
+        // create variable keys to fetch all the messages
+        const keys = Object.keys(messages);
 
+        // console.log(keys);
+        
+        // render the messages
+        return keys.map((key, index) => {
+            const message = messages[key];
+            const lastMessage = index === 0 ? null : keys[index - 1];
+            const isMyMessage = userName === message.sender.username;
+
+            return (
+                // create a div that will act as the 
+                // container for the messages
+                <div key={`msg_${index}`} style={{ width: '100%' }}>
+                    {/* create another div for the message itself */}
+                    <div className="message-block">
+                        
+                    </div>
+                </div>
+            )
+        })
     }
-    
+
+    // call renderMessages
+    renderMessages();
+
     return (
         <div>
             ChatFeed
