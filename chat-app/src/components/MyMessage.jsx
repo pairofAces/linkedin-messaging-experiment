c134b2b481b4d0f619a4116d700a9fbb0701af09
pairofAces <karanchauhan16@gmail.com>
2021-05-03 18:10:29 -0400

    creating code base for MyMessage and TheirMessage components

--- chat-app/src/components/MyMessage.jsx
+++ chat-app/src/components/MyMessage.jsx
@@ -12,7 +12,7 @@ const MyMessage = ({ message }) => {
 
     return (
         <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor= '#3b2a50'}}>
-            MyMessage
+            {message.text}
         </div>
     )
 }
--- chat-app/src/components/TheirMessage.jsx
+++ chat-app/src/components/TheirMessage.jsx
@@ -1,8 +1,32 @@
-const TheirMessage = () => {
+const TheirMessage = ({ lastMessage, message }) => {
+    // the following will provide a boolean value of whether or not the 
+    // first message is by the User
+    const isFirstMessageByUser = !lastMessage || 
+        lastMessage.sender.userName !== message.sender.userName;
+
 
     return (
-        <div>
-            TheirMessage
+        <div className="message-row">
+            {isFirstMessageByUser && (
+                <div
+                    className="message-avatar"
+                    style={{backgroundImage: `url(${message?.sender?.avatar})`}}
+                />
+            )}
+            {message?.attachments?.length > 0 
+                ? (
+                    <img 
+                        src={message.attachments[0].file}
+                        alt="message-attachment"
+                        className="message-image"
+                        style={{ float: 'right' }}
+                    />
+                ) : (
+                    <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor= '#3b2a50'}}>
+                        {message.text}
+                    </div>
+                )
+    }
         </div>
     )
 }
