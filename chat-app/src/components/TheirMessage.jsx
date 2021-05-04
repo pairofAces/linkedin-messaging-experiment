c355ed7eca862b43d50561d4ec4066cb588e6fae
pairofAces <karanchauhan16@gmail.com>
2021-05-04 19:23:05 -0400

    changed img tag and div container inputs, changed code layout for better readability

--- chat-app/src/components/TheirMessage.jsx
+++ chat-app/src/components/TheirMessage.jsx
@@ -19,10 +19,18 @@ const TheirMessage = ({ lastMessage, message }) => {
                         src={message.attachments[0].file}
                         alt="message-attachment"
                         className="message-image"
-                        style={{ float: 'right' }}
+                        style={{ 
+                            marginLeft: isFirstMessageByUser ? '4px' : '48px'
+                        }}
                     />
                 ) : (
-                    <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor= '#3b2a50'}}>
+                    <div 
+                        className="message" 
+                        style={{ 
+                            float: 'left', 
+                            backgroundColor= '#CABCDC',
+                            marginLeft: isFirstMessageByUser ? '4px' : '48px'
+                            }}>
                         {message.text}
                     </div>
                 )
