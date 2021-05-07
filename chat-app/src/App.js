2b041d9fa1c98b3d03a6f29d9d3d0b22fffd212d
pairofAces <karanchauhan16@gmail.com>
2021-05-06 22:59:23 -0400

    testing the perspective of the other user -> works fine

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -4,11 +4,20 @@
 
  const App = () => {
     return (
+        // <ChatEngine 
+        //     height="100vh"
+        //     projectID="cb3f0dee-0d92-43eb-baad-f7af7a9ce23c"
+        //     userName="chatTest"
+        //     userSecret="123"
+        //     renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+        // />
+
+        // testing the other user
         <ChatEngine 
             height="100vh"
             projectID="cb3f0dee-0d92-43eb-baad-f7af7a9ce23c"
-            userName="chatTest"
-            userSecret="123"
+            userName="zackzimmer"
+            userSecret="123123"
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
         />
     )
