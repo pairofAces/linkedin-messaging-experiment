d1f8f9c4cbdd14f149fc6780742cf567174c20f4
pairofAces <karanchauhan16@gmail.com>
2021-05-12 20:31:34 -0400

    working on creating the logic to set username and password, using localStorage

--- chat-app/src/components/LoginForm.jsx
+++ chat-app/src/components/LoginForm.jsx
@@ -5,7 +5,24 @@ const LoginForm = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    const submitHandler = () => {
+    const submitHandler = async (e) => {
+        e.preventDefault();
+
+        const authObject = { "Project-ID": "cb3f0dee-0d92-43eb-baad-f7af7a9ce23c", "User-Name": username, "User-Secret": password };
+        
+        try {
+            // username / password => chatengine -> will give messages
+            await axios.get('https://api.chatengine.io/chats', { headers: authObject });
+            
+            // if it works out -> then user will be logged in
+                // set the username and password -> using localStorage
+            localStorage.setItem('username', username);
+            localStorage.setItem('password', password);
+            
+        } catch (error) {
+            // otherwise, if there's an error -> user should try with a new username...
+
+        }
 
     };
 
