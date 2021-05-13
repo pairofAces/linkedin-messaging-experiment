1ef51446aab7ef63f6328b5db31d65642664d88c
pairofAces <karanchauhan16@gmail.com>
2021-05-12 20:16:51 -0400

    working on LoginForm component, imported useState and axios for fetching from API

--- chat-app/src/components/LoginForm.jsx
+++ chat-app/src/components/LoginForm.jsx
@@ -1,4 +1,15 @@
+import { useState } from 'react';
+import axios from ' axios';
+
 const LoginForm = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+
+    return (
+        <div className="wrapper">
+            <div className="form">
+
+            </div>
+        </div>
+    );
 }
