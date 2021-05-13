58b5a65d81e5029899f4f80b196024217bf77c1f
pairofAces <karanchauhan16@gmail.com>
2021-05-12 20:21:01 -0400

    created input fields for users to register

--- chat-app/src/components/LoginForm.jsx
+++ chat-app/src/components/LoginForm.jsx
@@ -5,10 +5,18 @@ const LoginForm = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
+    const submitHandler = () => {
+
+    };
+
     return (
         <div className="wrapper">
             <div className="form">
-
+                <h1 className="title">Chat Application</h1>
+                <form onSubmit={submitHandler}>
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required/>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required/>
+                </form>
             </div>
         </div>
     );
