4b122857de40970c39d43eeb6c9ce8f42ee6f8c7
pairofAces <karanchauhan16@gmail.com>
2021-05-13 22:12:44 -0400

    working on login, error handling

--- chat-app/src/components/LoginForm.jsx
+++ chat-app/src/components/LoginForm.jsx
@@ -4,6 +4,7 @@ import axios from ' axios';
 const LoginForm = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
 
     const submitHandler = async (e) => {
         e.preventDefault();
